f0a72a075fff9dab996091940db60e994b88587d
Samuel Machado <samuelmachado1@gmail.com>
2022-04-20 00:10:10 -0300

    [cineExpress] Adding encryption to the registration password and token return with JWT

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 router.get('/', (req, res, next) => {
@@ -16,35 +18,135 @@ router.get('/', (req, res, next) => {
           error: error
         });
       }
-      return res.status(200).send({ response: result });
+      const response = {
+        quantity: result.length,
+        request: {
+          type: 'GET',
+          description: 'Return all users'
+        },
+        users: result.map(user => {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+            url: 'http://localhost:3000/users/' + user.id
+          }
+        }),
+      };
+      return res.status(200).send({ response });
     }
     );
   });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/register', (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) {
       return res.status(500).send({
         error: error
       });
     }
-    conn.query('INSERT INTO users (name, cpf, birthdate, phone, city, state, email, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-      [req.body.name, req.body.cpf, req.body.birthdate, req.body.phone, req.body.city, req.body.state, req.body.email, req.body.password],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({
-            error: error
-          });
-        }
-        res.status(201).send({
-          message: 'Usuário criado com sucesso!',
-          id: result.insertId
+    conn.query(`SELECT * FROM users WHERE email = ?`, [req.body.email], (error, results) => {
+      if (results.length > 0) {
+        return res.status(409).send({
+          error: 'User already exists. Please, try again.'
         });
-      });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).send({
+              error: err
+            });
+          }
+          conn.query('INSERT INTO users (name, cpf, birthdate, phone, city, state, email, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+              req.body.name,
+              req.body.cpf,
+              req.body.birthdate,
+              req.body.phone,
+              req.body.city,
+              req.body.state,
+              req.body.email,
+              hash
+            ],
+            (error, results) => {
+              conn.release();
+              if (error) {
+                return res.status(500).send({
+                  error: error
+                });
+              }
+              response = {
+                message: 'Created user successfully!',
+                createdUser: {
+                  id: results.insertId,
+                  name: req.body.name,
+                },
+                request: {
+                  type: 'POST',
+                  description: 'Create a new user',
+                  url: 'http://localhost:3000/users',
+                }
+              }
+              return res.status(201).send(response);
+            });
+        });
+      }
+    });
   });
 });
+
+router.post('/login', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({
+        error: error
+      });
+    }
+    conn.query(`SELECT * FROM users WHERE email = ?`, [req.body.email], (error, results) => {
+      if (results.length < 1) {
+        return res.status(401).send({
+          error: 'Authentication failed. Please, try again.'
+        });
+      }
+      bcrypt.compare(req.body.password, results[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).send({
+            error: 'Authentication failed. Please, try again.'
+          });
+        }
+        if (result) {
+          const token = jwt.sign({
+            id: results[0].id,
+            name: results[0].name,
+            email: results[0].email,
+            created_at: results[0].created_at,
+            updated_at: results[0].updated_at
+          },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h"
+            }
+          );
+          return res.status(200).send({
+            message: 'Authentication successful!',
+            token: token
+          });
+        }
+        return res.status(401).send({
+          error: 'Authentication failed. Please, try again.'
+        });
+      });
+    });
+  });
+});
+
+
+
+
 
 router.get('/:userId', (req, res, next) => {
   mysql.getConnection((error, conn) => {
@@ -60,7 +162,24 @@ router.get('/:userId', (req, res, next) => {
           error: error
         });
       }
-      res.status(200).send({ response: result });
+      if (result.length == 0) {
+        return res.status(404).send({
+          message: 'User not found!'
+        });
+      }
+      const response = {
+        user: {
+          id: result[0].id,
+          name: result[0].name,
+          email: result[0].email,
+          request: {
+            type: 'GET',
+            description: 'Return a user',
+            url: 'http://localhost:3000/users/' + result[0].id
+          }
+        }
+      };
+      res.status(200).send({ response });
     }
     );
   });
@@ -82,9 +201,19 @@ router.patch('/', (req, res, next) => {
             error: error
           });
         }
-        res.status(200).send({
-          message: 'Usuário atualizado com sucesso!'
-        });
+        const response = {
+          message: 'Updated user successfully!',
+          userUpdated: {
+            id: req.body.id,
+            name: req.body.name,
+          },
+          request: {
+            type: 'PATCH',
+            description: 'Update a user',
+            url: 'http://localhost:3000/users/' + req.body.id
+          }
+        };
+        res.status(202).send(response);
       }
     );
   });
@@ -104,9 +233,14 @@ router.delete('/', (req, res, next) => {
           error: error
         });
       }
-      res.status(202).send({
-        message: 'Usuário deletado com sucesso!'
-      });
+      const response = {
+        message: 'Deleted user successfully!',
+        request: {
+          type: 'DELETE',
+          description: 'Delete a user'
+        }
+      };
+      res.status(202).send(response);
     }
     );
   });
